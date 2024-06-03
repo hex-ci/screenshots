d8b11b45377fe7638bd14c2e6bd2165d2573820f
Hex <zhaochang@openatom.org>
2024-06-03 16:16:42 +0000

    完善功能

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# screenshots
+# 批量网页截图演示项目
+
+## 安装依赖
+
+```
+pnpm i
+```
+
+## 用法
+
+```
+node index.js <源 xlsx 文件> <目标目录> --concurrency 并发数 --delay 延迟秒数 --name 输出文件名
+```
+
+* --concurrency: 并发数，默认是 5
+* --delay: 延迟多少秒截图，默认 2 秒
+* --name: 输出的 xlsx 文件名，默认是 `main`，自动添加 `.xlsx` 扩展名。
+
+## 示例用法
+
+```
+node index.js input.xlsx dest
+node index.js input.xlsx dest --concurrency 2 --delay 4 --name my
+```
---
+++ index.js
@@ -0,0 +1,95 @@
+import { join } from 'node:path';
+import { mkdirSync } from 'node:fs';
+import minimist from 'minimist';
+import Pageres from 'pageres';
+import Excel from 'exceljs';
+import PQueue from 'p-queue';
+
+const argv = minimist(process.argv.slice(2), {
+  string: ['_'],
+});
+
+const sourceFilename = argv._[0];
+const destPath = argv._[1];
+
+const concurrency = argv.concurrency || 5;
+const delay = argv.delay || 2;
+const destFilename = argv.name || 'main';
+
+if (argv._.length < 2) {
+  console.log('用法: node index.js <源 xlsx 文件> <目标目录>');
+  process.exit(1);
+}
+
+mkdirSync(destPath, { recursive: true });
+
+const workbookSource = new Excel.Workbook();
+const worksheetsSource = await workbookSource.xlsx.readFile(sourceFilename);
+const worksheetSource = worksheetsSource.getWorksheet(worksheetsSource.worksheets[0].id);
+
+const workbookDest = new Excel.Workbook();
+workbookDest.creator = 'NodeJS';
+workbookDest.lastModifiedBy = 'NodeJS';
+workbookDest.created = new Date();
+workbookDest.modified = new Date();
+workbookDest.lastPrinted = new Date();
+
+const worksheetDest = workbookDest.addWorksheet('网页表');
+
+worksheetDest.columns = [
+  {
+    key: 'title',
+    header: '标题',
+    width: 100,
+  },
+  {
+    key: 'url',
+    header: 'URL',
+    width: 100,
+  },
+];
+
+worksheetDest.getCell('A1').font = { bold: true };
+worksheetDest.getCell('A1').alignment = { vertical: 'middle', horizontal: 'center' };
+worksheetDest.getCell('B1').font = { bold: true };
+worksheetDest.getCell('B1').alignment = { vertical: 'middle', horizontal: 'center' };
+
+const dataToProcess = [];
+
+worksheetSource.eachRow((row) => {
+  const text = row.values[1];
+  const url = row.values[2];
+
+  dataToProcess.push({
+    url,
+    text: typeof text === 'object' ? text.text: text,
+  });
+});
+
+const queue = new PQueue({ concurrency });
+
+queue.on('idle', async () => {
+	await workbookDest.xlsx.writeFile(join(destPath, `${destFilename}.xlsx`));
+});
+
+dataToProcess.forEach((item) => {
+	queue.add(() => {
+    console.log(`Do: "${item.text}"`);
+
+    return new Pageres({
+      launchOptions: {
+        headless: 'new',
+      },
+      delay,
+      filename: `${item.text.replace(/\//g, '-')}`,
+    }).source(item.url, ['1920x1080']).destination(destPath).run().then((result) => {
+      worksheetDest.addRow([
+        { text: item.text, hyperlink: `${result[0].filename}` },
+        item.url,
+      ]).commit();
+      console.log(`Done: "${result[0].filename}"`);
+    }).catch(() => {
+      console.log(`Error: "${item.url}" "${item.text}"`);
+    });
+  });
+});
